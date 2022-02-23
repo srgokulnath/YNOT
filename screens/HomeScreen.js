0d000d74211cb5ac4f80e4614d519d46f44760e3
srgokulnath <srgwilet@gmail.com>
2022-02-23 18:41:32 +0530

    creating navigation from home screen to chat screen using navigation.navigate

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text,Button } from 'react-native'
 import React from 'react'
+import { useNavigation} from '@react-navigation/core'
 
 const HomeScreen = () => {
+
+    const navigation = useNavigation();
+
     return (
     <View>
         <Text>HomeScreen</Text>
+        <Button title = "Go to chat screen" onPress={() => navigation.navigate('Chat')}/>
     </View>
     );
 }
