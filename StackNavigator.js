2de09dd945fa6db34d6e5879f3733eac68bd28ea
srgokulnath <srgwilet@gmail.com>
2022-02-23 18:34:57 +0530

    created stack navigator with home and chat screens

---
+++ StackNavigator.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createNativeStackNavigator} from '@react-navigation/native-stack'
+import HomeScreen from './screens/HomeScreen';
+import ChatScreen from './screens/ChatScreen';
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+    return (
+    <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+    </Stack.Navigator>
+    );
+}
+
+export default StackNavigator
